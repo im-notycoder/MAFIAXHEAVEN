81388da541cf9cb1eb13f124fcf8d1bbbc78576e
ɪᴍ-ɴᴏᴛʏᴄᴏᴅᴇʀ <130166544+im-notycoder@users.noreply.github.com>
2024-11-12 20:21:41 +0530

    Update stickers.py

--- TanuMusic/plugins/tools/stickers.py
+++ TanuMusic/plugins/tools/stickers.py
@@ -1,123 +1,327 @@
-import base64
-import httpx
+import imghdr
+import math
 import os
-from pyrogram import filters
-from config import BOT_USERNAME
+from asyncio import gather
+from traceback import format_exc
+from typing import List
+
+from PIL import Image
+from pyrogram import Client, errors, filters, raw
+from pyrogram.errors import (
+    PeerIdInvalid,
+    ShortnameOccupyFailed,
+    StickerEmojiInvalid,
+    StickerPngDimensions,
+    StickerPngNopng,
+    UserIsBlocked,
+)
+from pyrogram.file_id import FileId
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+
 from TanuMusic import app
-from pyrogram import filters
-import pyrogram
-from uuid import uuid4
-from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup
+from utils.error import capture_err
+
+BOT_USERNAME = app.username
+
+MAX_STICKERS = (
+    120  # would be better if we could fetch this limit directly from telegram
+)
+SUPPORTED_TYPES = ["jpeg", "png", "webp"]
+STICKER_DIMENSIONS = (512, 512)
 
 
-EVAA = [
-    [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/DynamicMusicXBot?startgroup=true"),
-    ],
-]
-
-@app.on_message(filters.reply & filters.command("upscale"))
-async def upscale_image(client, message):
+async def get_sticker_set_by_name(
+    client: Client, name: str
+) -> raw.base.messages.StickerSet:
     try:
-        if not message.reply_to_message or not message.reply_to_message.photo:
-            await message.reply_text("<b>✦ ᴘʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴀɴ ɪᴍᴀɢᴇ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ɪᴛ. </b>")
-            return
-
-        image = message.reply_to_message.photo.file_id
-        file_path = await client.download_media(image)
-
-        with open(file_path, "rb") as image_file:
-            f = image_file.read()
-
-        b = base64.b64encode(f).decode("utf-8")
-
-        async with httpx.AsyncClient() as http_client:
-            response = await http_client.post(
-                "https://api.qewertyy.me/upscale", data={"image_data": b}, timeout=None
+        return await client.invoke(
+            raw.functions.messages.GetStickerSet(
+                stickerset=raw.types.InputStickerSetShortName(short_name=name),
+                hash=0,
             )
-
-        with open("upscaled_image.png", "wb") as output_file:
-            output_file.write(response.content)
-
-        await client.send_document(
-            message.chat.id,
-            document="upscaled_image.png",
-            caption="**๏ ʜᴇʀᴇ ɪs ᴛʜᴇ ᴜᴘsᴄᴀʟᴇᴅ ɪᴍᴀɢᴇ !**",
         )
+    except errors.exceptions.not_acceptable_406.StickersetInvalid:
+        return None
 
-    except Exception as e:
-        print(f"<b>✦ ғᴀɪʟᴇᴅ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ᴛʜᴇ ɪᴍᴀɢᴇ </b>: {e}")
-        await message.reply_text("<b>✦ ғᴀɪʟᴇᴅ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ᴛʜᴇ ɪᴍᴀɢᴇ. ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ. </b>")
 
-######### sticker id
+# Known errors: (I don't see a reason to catch them as we, for sure, won't face them right now):
+# errors.exceptions.bad_request_400.PackShortNameInvalid -> pack name needs to end with _by_botname
+# errors.exceptions.bad_request_400.ShortnameOccupyFailed -> pack's name
+# is already in use
+
+
+async def create_sticker_set(
+    client: Client,
+    owner: int,
+    title: str,
+    short_name: str,
+    stickers: List[raw.base.InputStickerSetItem],
+) -> raw.base.messages.StickerSet:
+    return await client.invoke(
+        raw.functions.stickers.CreateStickerSet(
+            user_id=await client.resolve_peer(owner),
+            title=title,
+            short_name=short_name,
+            stickers=stickers,
+        )
+    )
+
+
+async def add_sticker_to_set(
+    client: Client,
+    stickerset: raw.base.messages.StickerSet,
+    sticker: raw.base.InputStickerSetItem,
+) -> raw.base.messages.StickerSet:
+    return await client.invoke(
+        raw.functions.stickers.AddStickerToSet(
+            stickerset=raw.types.InputStickerSetShortName(
+                short_name=stickerset.set.short_name
+            ),
+            sticker=sticker,
+        )
+    )
+
+
+async def create_sticker(
+    sticker: raw.base.InputDocument, emoji: str
+) -> raw.base.InputStickerSetItem:
+    return raw.types.InputStickerSetItem(document=sticker, emoji=emoji)
+
+
+async def resize_file_to_sticker_size(file_path: str) -> str:
+    im = Image.open(file_path)
+    if (im.width, im.height) < STICKER_DIMENSIONS:
+        size1 = im.width
+        size2 = im.height
+        if im.width > im.height:
+            scale = STICKER_DIMENSIONS[0] / size1
+            size1new = STICKER_DIMENSIONS[0]
+            size2new = size2 * scale
+        else:
+            scale = STICKER_DIMENSIONS[1] / size2
+            size1new = size1 * scale
+            size2new = STICKER_DIMENSIONS[1]
+        size1new = math.floor(size1new)
+        size2new = math.floor(size2new)
+        sizenew = (size1new, size2new)
+        im = im.resize(sizenew)
+    else:
+        im.thumbnail(STICKER_DIMENSIONS)
+    try:
+        os.remove(file_path)
+        file_path = f"{file_path}.png"
+        return file_path
+    finally:
+        im.save(file_path)
+
+
+async def upload_document(
+    client: Client, file_path: str, chat_id: int
+) -> raw.base.InputDocument:
+    media = await client.invoke(
+        raw.functions.messages.UploadMedia(
+            peer=await client.resolve_peer(chat_id),
+            media=raw.types.InputMediaUploadedDocument(
+                mime_type=client.guess_mime_type(file_path) or "application/zip",
+                file=await client.save_file(file_path),
+                attributes=[
+                    raw.types.DocumentAttributeFilename(
+                        file_name=os.path.basename(file_path)
+                    )
+                ],
+            ),
+        )
+    )
+    return raw.types.InputDocument(
+        id=media.document.id,
+        access_hash=media.document.access_hash,
+        file_reference=media.document.file_reference,
+    )
+
+
+async def get_document_from_file_id(
+    file_id: str,
+) -> raw.base.InputDocument:
+    decoded = FileId.decode(file_id)
+    return raw.types.InputDocument(
+        id=decoded.media_id,
+        access_hash=decoded.access_hash,
+        file_reference=decoded.file_reference,
+    )
+
+
+@app.on_message(filters.command("stickerid"))
+@capture_err
+async def sticker_id(_, message: Message):
+    reply = message.reply_to_message
+
+    if not reply:
+        return await message.reply("Rᴇᴘʟʏ ᴛᴏ ᴀ sᴛɪᴄᴋᴇʀ.")
+
+    if not reply.sticker:
+        return await message.reply("Rᴇᴘʟʏ ᴛᴏ ᴀ sᴛɪᴄᴋᴇʀ.")
+
+    await message.reply_text(f"`{reply.sticker.file_id}`")
+
+
+@app.on_message(filters.command("getsticker"))
+@capture_err
+async def sticker_image(_, message: Message):
+    r = message.reply_to_message
+
+    if not r:
+        return await message.reply("Rᴇᴘʟʏ ᴛᴏ ᴀ sᴛɪᴄᴋᴇʀ.")
+
+    if not r.sticker:
+        return await message.reply("Rᴇᴘʟʏ ᴛᴏ ᴀ sᴛɪᴄᴋᴇʀ.")
+
+    m = await message.reply("Sending..")
+    f = await r.download(f"{r.sticker.file_unique_id}.png")
+
+    await gather(
+        *[
+            message.reply_photo(f),
+            message.reply_document(f),
+        ]
+    )
+
+    await m.delete()
+    os.remove(f)
+
 
 @app.on_message(filters.command("kang"))
-async def _packkang(app :app,message):  
-    txt = await message.reply_text("<b>✦ ᴘʀᴏᴄᴇssɪɴɢ.... </b>")
+@capture_err
+async def kang(client, message: Message):
     if not message.reply_to_message:
-        await txt.edit('ʀᴇᴘʟʏ ᴛᴏ ᴍᴇssᴀɢᴇ')
-        return
-    if not message.reply_to_message.sticker:
-        await txt.edit('ʀᴇᴘʟʏ ᴛᴏ sᴛɪᴄᴋᴇʀ')
-        return
-    if message.reply_to_message.sticker.is_animated or  message.reply_to_message.sticker.is_video:
-        return await txt.edit("ʀᴇᴘʟʏ ᴛᴏ ᴀ ɴᴏɴ-ᴀɴɪᴍᴀᴛᴇᴅ sᴛɪᴄᴋᴇʀ")
-    if len(message.command) < 2:
-        pack_name =  f'{message.from_user.first_name}_sticker_pack_by_@DynamicMusicXBot'
-    else :
-        pack_name = message.text.split(maxsplit=1)[1]
-    short_name = message.reply_to_message.sticker.set_name
-    stickers = await app.invoke(
-        pyrogram.raw.functions.messages.GetStickerSet(
-            stickerset=pyrogram.raw.types.InputStickerSetShortName(
-                short_name=short_name),
-            hash=0))
-    shits = stickers.documents
-    sticks = []
-    
-    for i in shits:
-        sex = pyrogram.raw.types.InputDocument(
-                id=i.id,
-                access_hash=i.access_hash,
-                file_reference=i.thumbs[0].bytes
-            )
-        
-        sticks.append(
-            pyrogram.raw.types.InputStickerSetItem(
-                document=sex,
-                emoji=i.attributes[1].alt
-            )
-        )
+        return await message.reply_text("Rᴇᴘʟʏ ᴛᴏ ᴀ sᴛɪᴄᴋᴇʀ/ɪᴍᴀɢᴇ ᴛᴏ ᴋᴀɴɢ ɪᴛ.")
+    if not message.from_user:
+        return await message.reply_text("Yᴏᴜ ᴀʀᴇ ᴀɴᴏɴ ᴀᴅᴍɪɴ, ᴋᴀɴɢ sᴛɪᴄᴋᴇʀs ɪɴ ᴍʏ ᴘᴍ.")
+    msg = await message.reply_text("Kᴀɴɢɪɴɢ sᴛɪᴄᴋᴇʀs..")
 
+    # Find the proper emoji
+    args = message.text.split()
+    if len(args) > 1:
+        sticker_emoji = str(args[1])
+    elif message.reply_to_message.sticker and message.reply_to_message.sticker.emoji:
+        sticker_emoji = message.reply_to_message.sticker.emoji
+    else:
+        sticker_emoji = "❤"
+
+    # Get the corresponding fileid, resize the file if necessary
+    doc = message.reply_to_message.photo or message.reply_to_message.document
     try:
-        short_name = f'stikcer_pack_{str(uuid4()).replace("-","")}_by_{app.me.username}'
-        user_id = await app.resolve_peer(message.from_user.id)
-        await app.invoke(
-            pyrogram.raw.functions.stickers.CreateStickerSet(
-                user_id=user_id,
-                title=pack_name,
-                short_name=short_name,
-                stickers=sticks,
+        if message.reply_to_message.sticker:
+            sticker = await create_sticker(
+                await get_document_from_file_id(
+                    message.reply_to_message.sticker.file_id
+                ),
+                sticker_emoji,
             )
-        )
-        await txt.edit(f"❖ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴋᴀɴɢᴇᴅ ʟɪɴᴋ !\n● ᴛᴏᴛᴀʟ sᴛɪᴄᴋᴇʀ ➥ {len(sticks)}",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ᴘᴀᴄᴋ ʟɪɴᴋ",url=f"http://t.me/addstickers/{short_name}")]]))
+        elif doc:
+            if doc.file_size > 10000000:
+                return await msg.edit("Fɪʟᴇ sɪᴢᴇ ᴛᴏᴏ ʟᴀʀɢᴇ.")
+
+            temp_file_path = await app.download_media(doc)
+            image_type = imghdr.what(temp_file_path)
+            if image_type not in SUPPORTED_TYPES:
+                return await msg.edit("Fᴏʀᴍᴀᴛ ɴᴏᴛ sᴜᴘᴘᴏʀᴛᴇᴅ! ({})".format(image_type))
+            try:
+                temp_file_path = await resize_file_to_sticker_size(temp_file_path)
+            except OSError as e:
+                await msg.edit_text("Sᴏᴍᴇᴛʜɪɴɢ ᴡʀᴏɴɢ ʜᴀᴘᴘᴇɴᴇᴅ.")
+                raise Exception(
+                    f"sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ ᴡʜɪʟᴇ ʀᴇsɪᴢɪɴɢ ᴛʜᴇ sᴛɪᴄᴋᴇʀ (at {temp_file_path}); {e}"
+                )
+            sticker = await create_sticker(
+                await upload_document(client, temp_file_path, message.chat.id),
+                sticker_emoji,
+            )
+            if os.path.isfile(temp_file_path):
+                os.remove(temp_file_path)
+        else:
+            return await msg.edit("Nᴏᴘᴇ, ᴄᴀɴ'ᴛ ᴋᴀɴɢ ᴛʜᴀᴛ.")
+    except ShortnameOccupyFailed:
+        await message.reply_text("ᴄʜᴀɴɢᴇ ʏᴏᴜʀ ɴᴀᴍᴇ ᴏʀ ᴜsᴇʀɴᴀᴍᴇ")
+        return
+
     except Exception as e:
-        await message.reply(str(e))
+        await message.reply_text(str(e))
+        e = format_exc()
+        return print(e)
+
+    # Find an available pack & add the sticker to the pack; create a new pack if needed
+    # Would be a good idea to cache the number instead of searching it every
+    # single time...
+    packnum = 0
+    packname = "f" + str(message.from_user.id) + "_by_" + BOT_USERNAME
+    limit = 0
+    try:
+        while True:
+            # Prevent infinite rules
+            if limit >= 50:
+                return await msg.delete()
+
+            stickerset = await get_sticker_set_by_name(client, packname)
+            if not stickerset:
+                stickerset = await create_sticker_set(
+                    client,
+                    message.from_user.id,
+                    f"{message.from_user.first_name[:32]}'s ᴋᴀɴɢ ᴘᴀᴄᴋ",
+                    packname,
+                    [sticker],
+                )
+            elif stickerset.set.count >= MAX_STICKERS:
+                packnum += 1
+                packname = (
+                    "f"
+                    + str(packnum)
+                    + "_"
+                    + str(message.from_user.id)
+                    + "_by_"
+                    + BOT_USERNAME
+                )
+                limit += 1
+                continue
+            else:
+                try:
+                    await add_sticker_to_set(client, stickerset, sticker)
+                except StickerEmojiInvalid:
+                    return await msg.edit("[ERROR]: INVALID_EMOJI_IN_ARGUMENT")
+            limit += 1
+            break
+
+        keyboard = InlineKeyboardMarkup([[InlineKeyboardButton(text="sᴇᴇ ᴘᴀᴄᴋ", url=f"t.me/addstickers/{packname}")]])
+        
+        await msg.edit(f"sᴛɪᴄᴋᴇʀ Kᴀɴɢᴇᴅ.\nʙʏ ᴅʏɴᴀᴍɪᴄ ᴍᴜsɪᴄ\nEᴍᴏᴊɪ: {sticker_emoji}", reply_markup=keyboard)
+            
+        
+    except (PeerIdInvalid, UserIsBlocked):
+        keyboard = InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text="Start", url=f"t.me/{BOT_USERNAME}")]]
+        )
+        await msg.edit(
+            "ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ sᴛᴀʀᴛ ᴀ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ ᴡɪᴛʜ ᴍᴇ.",
+            reply_markup=keyboard,
+        )
+    except StickerPngNopng:
+        await message.reply_text(
+            "sᴛɪᴄᴋᴇʀs ᴍᴜsᴛ ʙᴇ ᴘɴɢ ғɪʟᴇs ʙᴜᴛ ᴛʜᴇ ᴘʀᴏᴠɪᴅᴇᴅ ɪᴍᴀɢᴇ ᴡᴀs ɴᴏᴛ ᴀ ᴘɴɢ"
+        )
+    except StickerPngDimensions:
+        await message.reply_text("Tʜᴇ sᴛɪᴄᴋᴇʀ ᴘɴɢ ᴅɪᴍᴇɴsɪᴏɴs ᴀʀᴇ ɪɴᴠᴀʟɪᴅ.")
 
 
-###### sticker id =
-@app.on_message(filters.command(["stickerid","stid"]))
-async def sticker_id(app: app, msg):
-    if not msg.reply_to_message:
-        await msg.reply_text("✦ Reply to a sticker")        
-    elif not msg.reply_to_message.sticker:
-        await msg.reply_text("✦ Reply to a sticker")        
-    st_in = msg.reply_to_message.sticker
-    await msg.reply_text(f"""
-❖ sᴛɪᴄᴋᴇʀ ɪɴғᴏ ❖
-
-● sᴛɪᴄᴋᴇʀ ɪᴅ ➥ {st_in.file_id}\n
-● sᴛɪᴄᴋᴇʀ ᴜɴɪǫᴜᴇ ɪᴅ ➥ {st_in.file_unique_id}""", reply_markup=InlineKeyboardMarkup(EVAA),)
 
 
-#####
+__MODULE__ = "Sᴛɪᴄᴋᴇʀ"
+__HELP__ = """
+**COMMANDS:**
+
+• /stickerid - **ɢᴇᴛs ᴛʜᴇ ғɪʟᴇ ɪᴅ ᴏғ ᴀɴʏ ʀᴇᴘʟɪᴇᴅ sᴛɪᴄᴋᴇʀ.**
+• /getsticker - **ɢᴇᴛs ᴛʜᴇ ɪᴍᴀɢᴇ ᴏғ ᴀɴʏ ʀᴇᴘʟɪᴇᴅ sᴛɪᴄᴋᴇʀ.**
+• /kang - **ᴋᴀɴɢs ᴀɴʏ sᴛɪᴄᴋᴇʀ ɪɴ ᴛʜᴇ ʏᴏᴜ ᴘᴀᴄᴋ**
+
+**INFO:**
+
+- ᴛʜɪs ʙᴏᴛ ᴀʟʟᴏᴡs ᴜsᴇʀs ᴛᴏ ɢᴇᴛ ᴛʜᴇ ғɪʟᴇ ɪᴅ ᴏʀ ᴛʜᴇ ɪᴍᴀɢᴇ ᴏғ ᴀɴʏ sᴛɪᴄᴋᴇʀ ᴛʜᴀᴛ ɪs ʀᴇᴘʟɪᴇᴅ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ, ᴀɴᴅ ᴀʟsᴏ ᴀʟʟᴏᴡs ᴜsᴇʀs ᴛᴏ ᴋᴀɴɢ ᴀɴʏ sᴛɪᴄᴋᴇʀ ɪɴ ᴛʜᴇ ᴄʜᴀᴛ ᴀɴᴅ ᴀᴅᴅ ɪᴛ ᴛᴏ ᴀ sᴛɪᴄᴋᴇʀ ᴘᴀᴄᴋ.
+"""
